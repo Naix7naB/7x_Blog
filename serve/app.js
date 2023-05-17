b587b5c06993e4ac0aeffdabbf62c91b45a0d811
Naix7naB <duemarsgh@foxmail.com>
2023-05-17 19:29:51 +0800

    feat: 设置静态资源文件夹

--- serve/app.js
+++ serve/app.js
@@ -18,6 +18,8 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(express.static(path.join(root, 'public')))
+app.use(express.static(path.join(root, 'uploads')))
 
 app.use(jwtMiddleware())
 
