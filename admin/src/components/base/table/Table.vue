b57d8d749b6726802ed6bbe9c5a4b815da350417
Naix7naB <duemarsgh@foxmail.com>
2023-05-20 00:54:15 +0800

    feat: 公共 Table 组件

---
+++ admin/src/components/base/table/Table.vue
@@ -0,0 +1,51 @@
+<script>
+import { tableHeaderData, tableData } from './table.config'
+
+export default {
+  data() {
+    return {
+      activeName: 'released',
+      tableHeaderData,
+      tableData
+    }
+  }
+}
+</script>
+
+<template>
+  <el-card>
+    <template #header>
+      <slot name="table-header"></slot>
+    </template>
+    <template #default>
+      <el-tabs v-model="activeName">
+        <el-tab-pane label="已发布" name="released" />
+        <el-tab-pane label="草稿" name="draft" />
+      </el-tabs>
+      <el-table :data="tableData">
+        <el-table-column
+          v-for="item in tableHeaderData"
+          :key="item.field"
+          :prop="item.field"
+          :label="item.label"
+          :fixed="item.field === 'operation' && 'right'"
+          min-width="120"
+          align="center"
+          header-align="center"
+        />
+      </el-table>
+      <el-pagination background layout="total, prev, pager, next" :total="tableData.length" />
+    </template>
+  </el-card>
+</template>
+
+<style lang="scss" scoped>
+:deep(.el-card__header) {
+  border: none;
+}
+
+:deep(.el-pagination) {
+  margin-top: 24PX;
+  text-align: center;
+}
+</style>
---
+++ admin/src/components/base/table/table.config.js
@@ -0,0 +1,100 @@
+import { formatDate } from '@/utils/util'
+
+const tableHeaderData = [
+  {
+    field: 'articleTitle',
+    label: '文章标题'
+  },
+  {
+    field: 'articleCover',
+    label: '文章封面'
+  },
+  {
+    field: 'articleAuthor',
+    label: '文章作者'
+  },
+  {
+    field: 'articleViews',
+    label: '浏览量'
+  },
+  {
+    field: 'articleLikes',
+    label: '点赞数'
+  },
+  {
+    field: 'articleComments',
+    label: '评论数'
+  },
+  {
+    field: 'articleTag',
+    label: '文章标签'
+  },
+  {
+    field: 'createdTime',
+    label: '创建时间'
+  },
+  {
+    field: 'operation',
+    label: '操作'
+  }
+]
+
+const tableData = [
+  {
+    articleTitle: 'test_article1',
+    articleCover: 'corver_url1',
+    articleAuthor: 'Naixxx',
+    articleViews: '78',
+    articleLikes: '10',
+    articleComments: '5',
+    articleTag: '前端',
+    createdTime: formatDate(),
+    operation: ''
+  },
+  {
+    articleTitle: 'test_article2',
+    articleCover: 'corver_url2',
+    articleAuthor: 'Naixxx',
+    articleViews: '67',
+    articleLikes: '6',
+    articleComments: '3',
+    articleTag: '前端',
+    createdTime: formatDate(),
+    operation: ''
+  },
+  {
+    articleTitle: 'test_article3',
+    articleCover: 'corver_url3',
+    articleAuthor: 'Naixxx',
+    articleViews: '198',
+    articleLikes: '25',
+    articleComments: '29',
+    articleTag: '前端',
+    createdTime: formatDate(),
+    operation: ''
+  },
+  {
+    articleTitle: 'test_article4',
+    articleCover: 'corver_url4',
+    articleAuthor: 'Naixxx',
+    articleViews: '65',
+    articleLikes: '5',
+    articleComments: '6',
+    articleTag: '前端',
+    createdTime: formatDate(),
+    operation: ''
+  },
+  {
+    articleTitle: 'test_article5',
+    articleCover: 'corver_url5',
+    articleAuthor: 'Naixxx',
+    articleViews: '117',
+    articleLikes: '22',
+    articleComments: '17',
+    articleTag: '前端',
+    createdTime: formatDate(),
+    operation: ''
+  }
+]
+
+export { tableHeaderData, tableData }
