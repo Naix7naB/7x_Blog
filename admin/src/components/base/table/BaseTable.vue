e3daecc29e0fe83c4f45e9f73698cb07ae0f3f3e
Naix7naB <duemarsgh@foxmail.com>
2023-06-02 23:49:25 +0800

    feat: BaseTable 封装, 新增操作列可配置, 分页工具可配置

--- admin/src/components/base/table/BaseTable.vue
+++ admin/src/components/base/table/BaseTable.vue
@@ -2,13 +2,17 @@
 export default {
     name: 'BaseTable',
     props: {
-        tableHeader: {
+        columns: {
             type: Array,
             required: true
         },
-        tableContent: {
+        datasource: {
             type: Array,
             default: () => []
+        },
+        pageConfig: {
+            type: Object,
+            default: () => null
         },
         hasSelection: {
             type: Boolean,
@@ -20,11 +24,7 @@ export default {
         },
         hasPagination: {
             type: Boolean,
-            default: true
-        },
-        total: {
-            type: Number,
-            default: 0
+            default: false
         }
     },
     data() {
@@ -33,59 +33,76 @@ export default {
         }
     },
     methods: {
+        buttonClick(data, idx, act) {
+            this.$emit('handleButtonClick', { data, idx, act })
+        },
         changePage(page) {
-            console.log(page)
+            this.$emit('handlePageChange', page)
         }
     }
 }
 </script>
 
 <template>
-    <el-card>
-        <template #header>
-            <slot name="table-header"></slot>
-        </template>
-        <template #default>
-            <el-tabs v-model="activeName">
-                <el-tab-pane label="已发布" name="released" />
-                <el-tab-pane label="草稿" name="draft" />
-            </el-tabs>
-            <el-table :data="tableContent">
-                <!-- 选择列 -->
-                <el-table-column v-if="hasSelection" type="selection" width="55" />
-                <!-- 序号列 -->
-                <el-table-column v-if="hasIndex" type="index" width="55" />
-                <!-- 数据源 -->
-                <template v-for="item in tableHeader">
-                    <!-- 插槽列 -->
-                    <el-table-column v-if="item.type === 'slot'" v-bind="item" :key="item.prop">
-                        <template slot-scope="{ $index, row }">
-                            <slot :name="item.slotName" :row="row" :index="$index" />
-                        </template>
-                    </el-table-column>
-                    <!-- 普通列 -->
-                    <el-table-column v-else v-bind="item" :key="item.prop" />
-                </template>
-            </el-table>
-            <el-pagination
-                v-if="hasPagination"
-                layout="total, prev, pager, next"
-                :total="total"
-                background
-                hide-on-single-page
-                @current-change="changePage"
-            />
-        </template>
-    </el-card>
+    <div class="base-table">
+        <slot name="table-header"></slot>
+        <slot name="table-tabs"></slot>
+        <el-table :data="datasource">
+            <!-- 选择列 -->
+            <el-table-column v-if="hasSelection" type="selection" width="55" />
+            <!-- 序号列 -->
+            <el-table-column v-if="hasIndex" type="index" width="55" />
+            <!-- 数据源 -->
+            <template v-for="item in columns">
+                <!-- 插槽列 -->
+                <el-table-column v-if="item.type === 'slot'" v-bind="item" :key="item.prop">
+                    <template slot-scope="{ $index, row }">
+                        <slot :name="item.slotName" :row="row" :index="$index" />
+                    </template>
+                </el-table-column>
+                <!-- 操作列 -->
+                <el-table-column
+                    v-else-if="item.type === 'opt'"
+                    :label="item.label"
+                    :align="item.align"
+                    :fixed="item.fixed || false"
+                    :min-width="80 * item.options.length"
+                    :key="item.prop"
+                >
+                    <template slot-scope="{ row, $index }">
+                        <el-button
+                            v-for="btn in item.options"
+                            :type="btn.type"
+                            :size="btn.size"
+                            :data-act="btn.act"
+                            :key="btn.text"
+                            @click="buttonClick(row, $index, btn.act)"
+                        >
+                            {{ btn.text }}
+                        </el-button>
+                    </template>
+                </el-table-column>
+                <!-- 普通列 -->
+                <el-table-column v-else v-bind="item" :key="item.prop" />
+            </template>
+        </el-table>
+        <el-pagination
+            v-if="hasPagination"
+            layout="total, prev, pager, next"
+            :current-page="pageConfig.page"
+            :page-size="pageConfig.size"
+            :total="pageConfig.total"
+            :style="{ marginTop: '24px', textAlign: pageConfig.position || 'center' }"
+            :background="true"
+            @current-change="changePage"
+        />
+    </div>
 </template>
 
 <style lang="scss" scoped>
-:deep(.el-card__header) {
-    border: none;
-}
-
-:deep(.el-pagination) {
-    margin-top: 24PX;
-    text-align: center;
+.base-table {
+    padding: 30PX 40PX;
+    border-radius: 6PX;
+    background-color: #fff;
 }
 </style>
