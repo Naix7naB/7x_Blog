a30d33ddf5fe8a3ee74a5e5af728449a535e03e2
Naix7naB <duemarsgh@foxmail.com>
2023-06-03 01:21:21 +0800

    feat: 精简代码, 取消元素不必要的绑定信息

--- admin/src/components/base/table/BaseTable.vue
+++ admin/src/components/base/table/BaseTable.vue
@@ -10,7 +10,7 @@ export default {
             type: Array,
             default: () => []
         },
-        pageConfig: {
+        pagerConfig: {
             type: Object,
             default: () => null
         },
@@ -27,17 +27,12 @@ export default {
             default: false
         }
     },
-    data() {
-        return {
-            activeName: 'released'
-        }
-    },
     methods: {
         buttonClick(data, idx, act) {
-            this.$emit('handleButtonClick', { data, idx, act })
+            this.$emit('handleTableButtonClick', { data, idx, act })
         },
         changePage(page) {
-            this.$emit('handlePageChange', page)
+            this.$emit('handleTablePageChange', page)
         }
     }
 }
@@ -53,7 +48,7 @@ export default {
             <!-- 序号列 -->
             <el-table-column v-if="hasIndex" type="index" width="55" />
             <!-- 数据源 -->
-            <template v-for="item in columns">
+            <template v-for="{ options, ...item } in columns">
                 <!-- 插槽列 -->
                 <el-table-column v-if="item.type === 'slot'" v-bind="item" :key="item.prop">
                     <template slot-scope="{ $index, row }">
@@ -63,19 +58,15 @@ export default {
                 <!-- 操作列 -->
                 <el-table-column
                     v-else-if="item.type === 'opt'"
-                    :label="item.label"
-                    :align="item.align"
-                    :fixed="item.fixed || false"
-                    :min-width="80 * item.options.length"
+                    v-bind="item"
+                    :min-width="80 * options.length"
                     :key="item.prop"
                 >
                     <template slot-scope="{ row, $index }">
                         <el-button
-                            v-for="btn in item.options"
-                            :type="btn.type"
-                            :size="btn.size"
-                            :data-act="btn.act"
-                            :key="btn.text"
+                            v-for="btn in options"
+                            v-bind="btn"
+                            :key="btn.act"
                             @click="buttonClick(row, $index, btn.act)"
                         >
                             {{ btn.text }}
@@ -89,10 +80,10 @@ export default {
         <el-pagination
             v-if="hasPagination"
             layout="total, prev, pager, next"
-            :current-page="pageConfig.page"
-            :page-size="pageConfig.size"
-            :total="pageConfig.total"
-            :style="{ marginTop: '24px', textAlign: pageConfig.position || 'center' }"
+            :current-page="pagerConfig.page"
+            :page-size="pagerConfig.size"
+            :total="pagerConfig.total"
+            :style="{ marginTop: '24px', textAlign: pagerConfig.position || 'center' }"
             :background="true"
             @current-change="changePage"
         />
