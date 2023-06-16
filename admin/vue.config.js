c1c442f4a800c0c2b737e6a9462ffb0417d3918c
Naix7naB <duemarsgh@foxmail.com>
2023-06-16 19:40:25 +0800

    chore: vue-cli配置

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -1,5 +1,27 @@
 const { defineConfig } = require('@vue/cli-service')
+
+const path = require('path')
+
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
+
 module.exports = defineConfig({
+    publicPath: '/',
+    outputDir: 'dist',
+    assetsDir: 'static',
+    lintOnSave: process.env.NODE_ENV === 'development',
+    productionSourceMap: false,
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@': resolve('src')
+            },
+            fallback: {
+                path: require.resolve('path-browserify')
+            }
+        }
+    },
     transpileDependencies: true,
     css: {
         loaderOptions: {
