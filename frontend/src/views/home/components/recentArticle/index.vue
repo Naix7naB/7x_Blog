7c586481305cd9bc012503680c0139ac96331c57
Naix7naB <duemarsgh@foxmail.com>
2023-08-01 05:55:17 +0800

    feat: 主页文章列表样式和布局

---
+++ frontend/src/views/home/components/recentArticle/index.vue
@@ -0,0 +1,185 @@
+<script>
+import inset from '@/assets/images/inset.png'
+
+export default {
+    data() {
+        return {
+            inset
+        }
+    }
+}
+</script>
+
+<template>
+    <ul class="recent-article--wrapper">
+        <li class="recent-article--item" v-for="item in 6" :key="item">
+            <div
+                class="article-item--wrapper recent-article--image"
+                :class="{ left: item % 2 === 1, right: item % 2 === 0 }"
+            >
+                <el-image :src="inset" fit="cover" />
+            </div>
+            <div
+                class="article-item--wrapper recent-article--info"
+                :class="{ left: item % 2 === 0, right: item % 2 === 1 }"
+            >
+                <div class="article-info--meta date">
+                    <fa-icon icon="fas fa-calendar-days" /> 发布于 2023-07-22 20:16:27
+                </div>
+                <h3 class="article-info--title">文章标题;文章标题;文章标题</h3>
+                <div class="article-info--meta data">
+                    <span><fa-icon icon="fas fa-fire" /> 1021</span>
+                    <span><fa-icon icon="fas fa-comment" /> 18</span>
+                    <span><fa-icon icon="fas fa-heart" /> 32</span>
+                </div>
+                <p class="article-info--desc">
+                    《百年孤独》，是哥伦比亚作家加西亚·马尔克斯创作的长篇小说，是其代表作，也是拉丁美洲魔幻现实主义文学的代表作，被誉为“再现拉丁美洲历史社会图景的鸿篇巨著”。
+                </p>
+                <div class="article-info--label">
+                    <span class="article-label--item"><fa-icon icon="fas fa-tag" /> 新闻</span>
+                    <span class="article-label--item"><fa-icon icon="fas fa-tag" /> 笔记</span>
+                </div>
+            </div>
+        </li>
+    </ul>
+</template>
+
+<style lang="scss" scoped>
+:deep(.el-image) {
+    width: 100%;
+    height: 100%;
+}
+
+:deep(.el-image__inner) {
+    transform: scale(1);
+    transition: transform .7s ease;
+}
+
+.recent-article--wrapper {
+    width: 70%;
+    max-width: 980PX;
+}
+
+.recent-article--item {
+    overflow: hidden;
+    position: relative;
+    display: flex;
+    height: 300PX;
+    margin-bottom: 40PX;
+    border-radius: 10PX;
+    background-color: #24272d;
+    box-shadow: 0 1PX 20PX -6PX rgba(0,0,0,0.5);
+    transition: box-shadow .3s ease;
+    cursor: pointer;
+
+    &:hover {
+        box-shadow: 0 5PX 10PX 5PX rgba(78, 85, 98, 0.4);
+    }
+
+    &:hover :deep(.el-image__inner) {
+        transform: scale(1.2);
+    }
+}
+
+.article-item--wrapper {
+    position: absolute;
+    top: 0;
+    height: 100%;
+
+    &.left {
+        left: 0;
+    }
+
+    &.right {
+        right: 0;
+        text-align: right;
+    }
+
+    &.right .article-info--label {
+        right: 36PX;
+    }
+}
+
+.recent-article--image {
+    width: 40%;
+}
+
+.recent-article--info {
+    width: 60%;
+    padding: 24PX 36PX;
+}
+
+.article-info--meta {
+    @include nowrap();
+    font-size: $fz-small;
+
+    &.data {
+        margin-bottom: 14PX;
+
+        &>span:not(:first-of-type) {
+            margin-left: 12PX;
+        }
+    }
+}
+
+.article-info--title {
+    @include nowrap();
+    margin: 22PX 0;
+    font-size: $fz-large;
+}
+
+.article-info--desc {
+    @include nowrap-multiple(4);
+    line-height: 1.5;
+}
+
+.article-info--label {
+    position: absolute;
+    bottom: 24PX;
+}
+
+.article-label--item {
+    line-height: 20PX;
+    padding: 2PX 12PX;
+    border-radius: 3PX;
+    font-size: $fz-small;
+    color: #797979;
+    background-color: #eeeeee;
+    cursor: pointer;
+
+    &:not(:first-of-type) {
+        margin-left: 12PX;
+    }
+
+    &:hover {
+        background-color: orange;
+        color: #ffffff;
+    }
+}
+
+@media screen and (max-width: 1100PX) {
+    .recent-article--wrapper {
+        width: 100%;
+        max-width: 100%;
+    }
+
+    .recent-article--item {
+        flex-direction: column;
+        height: 500PX;
+    }
+
+    .article-item--wrapper {
+        position: static;
+        width: 100%;
+    }
+
+    .recent-article--image {
+        height: 200PX;
+    }
+
+    .recent-article--info {
+        position: relative;
+        height: 300PX;
+    }
+}
+</style>
--- frontend/src/views/home/index.vue
+++ frontend/src/views/home/index.vue
@@ -1,5 +1,9 @@
 <script>
+import AsideArea from './components/asideArea'
+import RecentArticle from './components/recentArticle'
+
 export default {
+    components: { AsideArea, RecentArticle },
     data() {
         return {}
     }
@@ -7,7 +11,27 @@ export default {
 </script>
 
 <template>
-    <div>Home</div>
+    <section class="home-page">
+        <div class="home-page--container">
+            <AsideArea />
+            <RecentArticle />
+        </div>
+    </section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.home-page--container {
+    display: flex;
+    justify-content: center;
+    width: 90%;
+    margin: 0 auto;
+    padding: 40PX;
+}
+
+@media screen and (max-width: 1100PX) {
+    .home-page--container {
+        flex-direction: column;
+        width: 100%;
+    }
+}
+</style>
