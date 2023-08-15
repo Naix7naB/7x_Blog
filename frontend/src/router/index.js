e95f200a248a7a6c490a94fd1149a068dd3d6f90
Naix7naB <duemarsgh@foxmail.com>
2023-08-16 00:29:07 +0800

    feat: 添加路由

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,16 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import store from '@/store'
 import Layout from '@/layout'
 
 Vue.use(VueRouter)
 
 const routes = [
     {
-        path: '/',
-        redirect: '/intro'
-    },
-    {
+        hide: true,
         path: '/intro',
         name: 'Intro',
         component: () => import('@/views/intro')
@@ -23,7 +21,87 @@ const routes = [
             {
                 path: 'index',
                 name: 'Home',
+                meta: {
+                    icon: 'home',
+                    title: '主页'
+                },
                 component: () => import('@/views/home')
+            }
+        ]
+    },
+    {
+        path: '/classify',
+        redirect: '/classify/index',
+        component: Layout,
+        children: [
+            {
+                path: 'index',
+                name: 'Classify',
+                meta: {
+                    icon: 'folder-open',
+                    title: '分类'
+                },
+                component: () => import('@/views/home')
+            }
+        ]
+    },
+    {
+        path: '/tag',
+        redirect: '/tag/index',
+        component: Layout,
+        children: [
+            {
+                path: 'index',
+                name: 'Tag',
+                meta: {
+                    icon: 'bookmark',
+                    title: '标签'
+                },
+                component: () => import('@/views/home')
+            }
+        ]
+    },
+    {
+        path: '/archive',
+        redirect: '/archive/index',
+        component: Layout,
+        children: [
+            {
+                path: 'index',
+                name: 'Archive',
+                meta: {
+                    icon: 'box-archive',
+                    title: '归档'
+                },
+                component: () => import('@/views/home')
+            }
+        ]
+    },
+    {
+        path: '/message',
+        redirect: '/message/index',
+        component: Layout,
+        children: [
+            {
+                path: 'index',
+                name: 'message',
+                meta: {
+                    icon: 'message',
+                    title: '留言'
+                },
+                component: () => import('@/views/home')
+            }
+        ]
+    },
+    {
+        hide: true,
+        path: '/article',
+        component: Layout,
+        children: [
+            {
+                path: ':aid',
+                name: 'Article',
+                component: () => import('@/views/article')
             }
         ]
     },
@@ -32,6 +110,11 @@ const routes = [
         path: '/login',
         name: 'Login',
         component: () => import('@/views/login')
+    },
+    {
+        hide: true,
+        path: '/',
+        redirect: '/intro'
     }
 ]
 
