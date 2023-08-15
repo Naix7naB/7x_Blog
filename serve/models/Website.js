436fdc94f2dba751fac25da64cf0264a2bf4c938
Naix7naB <duemarsgh@foxmail.com>
2023-08-16 04:17:51 +0800

    feat: 创建新集合, 用于存储网站信息

---
+++ serve/models/Website.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const schemaOption = require('../config/schemaOption.config')
+
+const { resolveUrl } = require('../utils/helpers')
+
+const schema = new mongoose.Schema(
+    {
+        host: {
+            ref: 'User',
+            type: mongoose.SchemaTypes.ObjectId
+        },
+        name: {
+            type: String,
+            required: [true, '请输入网站名称']
+        },
+        logo: {
+            type: String,
+            default: 'fa-blog'
+        },
+        slogan: {
+            type: String,
+            default: 'slogan'
+        },
+        background: {
+            type: String,
+            default: '/website/default_background.png',
+            get(url) {
+                return resolveUrl(url)
+            }
+        },
+        article_num: 0,
+        classify_num: 0,
+        tag_num: 0,
+        created_at: Number,
+        updated_at: Number
+    },
+    schemaOption
+)
+
+module.exports = mongoose.model('Website', schema)
