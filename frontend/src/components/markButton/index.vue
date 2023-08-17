5689f82650b1797de3e1acce8818b6e5083d6798
Naix7naB <duemarsgh@foxmail.com>
2023-08-18 03:44:17 +0800

    feat: 用于标记文章分类和标签的小按钮

---
+++ frontend/src/components/markButton/index.vue
@@ -0,0 +1,78 @@
+<script>
+export default {
+    functional: true,
+    name: 'MarkButton',
+    props: {
+        type: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            default: ''
+        }
+    },
+    render(h, ctx) {
+        const { type, text } = ctx.props
+        const children = []
+        if (type) {
+            children.push(
+                <fa-icon class={['mark-button--icon', type]} icon={['fas', type === 'tag' ? 'tag' : 'folder-open']} />
+            )
+        }
+        if (text) {
+            children.push(
+                <span>{ text }</span>
+            )
+        }
+        if (!text && ctx.slots().default) {
+            children.push(
+                ctx.slots().default.map(vnode => {
+                    if (!vnode.tag) {
+                        return (
+                            <span>{ vnode.text.trim() }</span>
+                        )
+                    }
+                    return vnode
+                })
+            )
+        }
+
+        return h('span', { class: 'mark-button' }, children)
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.mark-button {
+    padding: 2PX 12PX;
+    line-height: 20PX;
+    border-radius: 2PX;
+    font-size: $fz-small;
+    color: #777;
+    background-color: #ddd;
+    transition: all  .2s;
+    cursor: pointer;
+
+    &:not(:first-of-type) {
+        margin-left: 10px;
+    }
+
+    &:hover {
+        color: #eee;
+        background-color: orange;
+    }
+}
+
+.mark-button--icon {
+    margin-right: 4px;
+}
+
+.mark-button--icon.classify {
+    color: #ffd977;
+}
+
+.mark-button--icon.tag {
+    color: #8c7bfd;
+}
+</style>
