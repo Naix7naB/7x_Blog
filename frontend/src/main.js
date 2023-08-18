d806ed841b8ab2bef5d0c627d503267ba0045dfb
Naix7naB <duemarsgh@foxmail.com>
2023-08-19 04:00:00 +0800

    chore: 入口文件引入directive.js和plugins.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,17 @@
-import '@/assets/scss/index.scss'
-
-import '@/plugins'
+import './assets/scss/index.scss'
 
 import Vue from 'vue'
 import router from './router'
 import store from './store'
 import App from './App.vue'
 
+import directive from './directive'
+import plugins from './plugins'
+
 Vue.config.productionTip = false
+
+Vue.use(directive)
+Vue.use(plugins)
 
 new Vue({
     router,
