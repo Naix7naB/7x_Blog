3069d2b4a854e742d496dc67160110093a3093ee
Naix7naB <duemarsgh@foxmail.com>
2023-08-22 01:18:51 +0800

    feat: 公共组件 InfoCard

---
+++ frontend/src/components/infoCard/index.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+    name: 'InfoCard',
+    props: {
+        iconType: {
+            type: String,
+            default: 'solid'
+        },
+        iconName: {
+            type: String,
+            default: ''
+        },
+        label: {
+            type: String,
+            default: ''
+        },
+        customClass: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
+
+<template>
+    <div :class="['info-card', customClass]">
+        <div v-if="label && iconName" class="info-card--label">
+            <fa-icon :icon="[iconType === 'solid' ? 'fas' : 'far', iconName]" />
+            <span style="margin-left: 6px;">{{ label }}</span>
+        </div>
+        <slot name="default"></slot>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+/* 卡片样式 */
+.info-card {
+    padding: 24px;
+    border-radius: 10px;
+    background-color: #24272d;
+}
+
+.info-card--label {
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+    line-height: $lh-medium;
+    font-size: $fz-medium-x;
+    font-weight: 700;
+}
+</style>
