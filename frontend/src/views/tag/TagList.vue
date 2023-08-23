b86f210009022b16cf32073924e4af5f15034903
Naix7naB <duemarsgh@foxmail.com>
2023-08-24 05:03:19 +0800

    feat: 展示所有标签页面

---
+++ frontend/src/views/tag/TagList.vue
@@ -0,0 +1,54 @@
+<script>
+import Hashtag from '@/components/hashtag'
+
+import { getTagList } from '@/apis/tag'
+
+export default {
+    name: 'ClassifyList',
+    components: { Hashtag },
+    data() {
+        return {
+            tags: []
+        }
+    },
+    methods: {
+        choose({ tagId, tagName }) {
+            this.$router.push({
+                name: 'TagArticle',
+                params: { tagId, tagName }
+            })
+        }
+    },
+    created() {
+        getTagList().then(({ data }) => {
+            this.tags = data.list
+            this.$bus.$emit('assignTitle', this.$route.meta.secondTitle)
+        }).catch(err => {
+            this.$message.error(err.errMsg)
+        })
+    }
+}
+</script>
+
+<template>
+    <div class="tag-list">
+        <Hashtag
+            v-for="tag in tags"
+            :key="tag.id"
+            :tag-id="tag.id"
+            :tag-name="tag.name"
+            :color="tag.color"
+            :count="tag.articles.length"
+            @choose="choose"
+        />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.tag-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    padding: 40px;
+}
+</style>
