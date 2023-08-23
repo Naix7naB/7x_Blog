2b0679e74cb09b554ad13c341f1cdd3092518fba
Naix7naB <duemarsgh@foxmail.com>
2023-08-24 05:11:57 +0800

    feat: 展示所属分类的文章列表样式

---
+++ frontend/src/views/classify/ClassifyArticle.vue
@@ -0,0 +1,37 @@
+<script>
+import ArticleList from '@/components/articleList'
+
+import { getClassifyArticles } from '@/apis/article'
+
+export default {
+    name: 'ClassifyPage',
+    components: {  ArticleList },
+    data() {
+        return {
+            articles: []
+        }
+    },
+    created() {
+        const { classifyId, classifyName } = this.$route.params
+        getClassifyArticles({ classify_id: classifyId }).then(({ data }) => {
+            this.articles = data.list
+            this.$bus.$emit('assignTitle', classifyName)
+        }).catch(err => {
+            this.$message.error(err.errMsg)
+        })
+    }
+}
+</script>
+
+<template>
+    <div class="classify-article">
+        <ArticleList :list="articles" />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+/* 分类文章列表样式 */
+.classify-article {
+    padding: 40px;
+}
+</style>
