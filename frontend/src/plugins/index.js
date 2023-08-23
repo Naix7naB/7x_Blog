dc84cb8bfbf08e1e438da73c257b83afbfbccf4c
Naix7naB <duemarsgh@foxmail.com>
2023-08-23 15:33:21 +0800

    feat: Layout中滚动容器改为全局注册

--- frontend/src/plugins/index.js
+++ frontend/src/plugins/index.js
@@ -1,8 +1,10 @@
 import './elementUI'
 import FontAwesomeIcon from './faIcon'
+import { vuescroll, config } from './vuescroll'
 
 export default {
     install(Vue) {
         Vue.component('fa-icon', FontAwesomeIcon)
+        Vue.use(vuescroll, config)
     }
 }
---
+++ frontend/src/plugins/vuescroll.js
@@ -0,0 +1,36 @@
+import vuescroll from 'vuescroll/dist/vuescroll-native'
+
+const config = {
+    name: 'v-scroll',
+    ops: {
+        vuescroll: {
+            mode: 'native',
+            sizeStrategy: 'percent',
+            renderMethod: 'position',
+            detectResize: false,
+            paging: false,
+            zooming: false,
+            wheelDirectionReverse: true,
+            locking: true
+        },
+        scrollPanel: {
+            scrollingX: false,
+            scrollingY: true,
+            easing: 'easeInOutCubic'
+        },
+        rail: {
+            opacity: 0
+        },
+        bar: {
+            keepShow: false,
+            onlyShowBarOnScroll: true,
+            background:
+                '#49b1f5 linear-gradient(45deg,rgba(255,255,255,.4) 25%,transparent 25%,transparent 50%,rgba(255,255,255,.4) 50%,rgba(255,255,255,.4) 75%,transparent 75%,transparent)'
+        },
+        scrollButton: {
+            enable: false
+        }
+    }
+}
+
+export { vuescroll, config }
