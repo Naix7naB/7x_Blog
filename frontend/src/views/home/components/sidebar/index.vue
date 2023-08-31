7103395ea2e8697f4164533c3f814f721589ed7d
Naix7naB <duemarsgh@foxmail.com>
2023-08-31 19:22:05 +0800

    chore: 侧边栏图标样式

--- frontend/src/views/home/components/sidebar/index.vue
+++ frontend/src/views/home/components/sidebar/index.vue
@@ -42,7 +42,7 @@ export default {
                 {
                     name: 'Tag',
                     icon: {
-                        type: 'regular',
+                        type: 'solid',
                         name: 'bookmark'
                     },
                     label: '标签',
