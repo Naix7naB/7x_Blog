51cdb57ba221db40cc95a07e2fcc8e1edf2de9b1
Naix7naB <duemarsgh@foxmail.com>
2023-09-02 16:27:57 +0800

    chore: 修改vuescroll的全局配置

--- frontend/src/plugins/vuescroll.js
+++ frontend/src/plugins/vuescroll.js
@@ -1,31 +1,33 @@
 import vuescroll from 'vuescroll'
 
 const config = {
-    name: 'v-scroll',
+    name: 'vuescroll',
     ops: {
         vuescroll: {
             mode: 'native',
-            sizeStrategy: 'percent',
-            renderMethod: 'position',
-            detectResize: false,
+            detectResize: true,
             paging: false,
             zooming: false,
             wheelDirectionReverse: true,
+            wheelScrollDuration: 0,
             locking: true
         },
         scrollPanel: {
             scrollingX: false,
             scrollingY: true,
+            speed: 500,
             easing: 'easeInOutCubic'
         },
         rail: {
-            opacity: 0
+            keepShow: false,
+            opacity: 0.4,
+            background: '#255f86'
         },
         bar: {
             keepShow: false,
             onlyShowBarOnScroll: true,
             background:
-                '#49b1f5 linear-gradient(45deg,rgba(255,255,255,.4) 25%,transparent 25%,transparent 50%,rgba(255,255,255,.4) 50%,rgba(255,255,255,.4) 75%,transparent 75%,transparent)'
+                '#2297e5 linear-gradient(45deg, rgba(255,255,255,.3) 25%, transparent 25%, transparent 50%, rgba(255,255,255,.3) 50%, rgba(255,255,255,.3) 75%, transparent 75%, transparent)'
         },
         scrollButton: {
             enable: false
