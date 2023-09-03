c34787400a82b476aec0b67ac3abaa78af1788c1
Naix7naB <duemarsgh@foxmail.com>
2023-09-03 18:51:24 +0800

    refactor: EmojiPicker组件使用emoji-mart-vue-fast重构

--- frontend/src/components/emojiPicker/index.vue
+++ frontend/src/components/emojiPicker/index.vue
@@ -1,114 +1,71 @@
 <script>
-import VueEmojiPicker from 'vue-emoji-picker'
+import { Picker as EmojiMartPicker, EmojiIndex } from 'emoji-mart-vue-fast'
+import EmojiTwitter from 'emoji-mart-vue-fast/data/twitter.json'
+import 'emoji-mart-vue-fast/css/emoji-mart.css'
 
 export default {
+    functional: true,
     name: 'EmojiPicker',
-    components: { VueEmojiPicker },
-    methods: {
-        insert2text(emoji) {
-            this.$emit('insert', emoji)
+    props: {
+        showCategory: {
+            type: Boolean,
+            default: true
+        },
+        showSearch: {
+            type: Boolean,
+            default: true
+        },
+        pickerStyle: {
+            type: Object,
+            default: () => {
+                return {
+                    width: '100%',
+                    height: '100%'
+                }
+            }
         }
+    },
+    render(h, ctx) {
+        const { showCategory, showSearch, pickerStyle } = ctx.props
+        const i18n = {
+            search: '搜索',
+            notfound: '没有找到对应表情',
+            categories: {
+                search: '搜索结果',
+                recent: '最近使用',
+                smileys: '心情',
+                people: '人物',
+                nature: '动物与大自然',
+                foods: '食物与饮料',
+                activity: '活动',
+                places: '旅行与地点',
+                objects: '物品',
+                symbols: '符号',
+                flags: '旗',
+                custom: '自定义'
+            }
+        }
+
+        const emojiData = new EmojiIndex(EmojiTwitter)
+
+        return h(EmojiMartPicker, {
+            props: {
+                set: 'twitter',
+                data: emojiData,
+                emojiTooltip: true,
+                i18n: i18n,
+                pickerStyles: pickerStyle,
+                showCategories: showCategory,
+                showPreview: false,
+                showSearch: showSearch,
+                showSkinTones: false
+            },
+            on: {
+                select(emoji) {
+                    ctx.listeners.emoji(emoji.native)
+                }
+            }
+        })
     }
 }
 </script>
-
-<template>
-    <vue-emoji-picker class="emoji-wrapper" @emoji="insert2text">
-        <div class="emoji-invoker" slot="emoji-invoker" slot-scope="{ events: { click: open } }">
-            <fa-icon @click.stop="open" icon="far fa-face-laugh" />
-        </div>
-        <ul class="emoji-picker" slot="emoji-picker" slot-scope="{ emojis, insert }">
-            <li v-for="(emojiGroup, category) in emojis" :key="category">
-                <p class="emoji-category">{{category}}</p>
-                <div class="emoji-content">
-                    <span
-                        v-for="(emoji, emojiName) in emojiGroup"
-                        :key="emojiName"
-                        :title="emojiName"
-                        @click="insert(emoji)"
-                    >
-                        {{ emoji }}
-                    </span>
-                </div>
-            </li>
-        </ul>
-    </vue-emoji-picker>
-</template>
-
-<style lang="scss" scoped>
-/* emoji表情容器样式 */
-.emoji-wrapper {
-    user-select: none;
-    position: relative;
-}
-
-.emoji-invoker {
-    transition: transform .2s;
-    cursor: pointer;
-
-    &:hover {
-        transform: scale(1.1);
-    }
-}
-
-.emoji-picker {
-    overflow: hidden scroll;
-    z-index: 100;
-    position: absolute;
-    top: 140%;
-    left: 140%;
-    width: 240px;
-    height: 320px;
-    padding: 12px 0 12px 12px;
-    border: 1px solid #ccc;
-    border-radius: 6px;
-    background: #fff;
-    box-shadow: 1px 1px 8px #c7dbe6;
-
-    &::-webkit-scrollbar {
-        width: 10px;
-    }
-
-    &::-webkit-scrollbar-thumb {
-        border-radius: 6px;
-        background-color: #c1c1c1;
-    }
-
-    &::-webkit-scrollbar-track {
-        border-radius: 6px;
-        background-color: #f1f1f1;
-    }
-
-    &::-webkit-scrollbar-corner {
-        background-color: transparent;
-    }
-}
-
-.emoji-picker li ~ li {
-    margin-top: 12px;
-}
-
-.emoji-category {
-    margin-bottom: 6px;
-    font-size: $fz-large;
-    color: #aaaaaa;
-}
-
-.emoji-content {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, 32px);
-    grid-auto-rows: 30px;
-    justify-items: center;
-    align-items: center;
-}
-
-.emoji-content span {
-    padding: 6px;
-    border-radius: 4px;
-    cursor: pointer;
-
-    &:hover {
-        background-color: #ececec;
-    }
-}
-</style>
