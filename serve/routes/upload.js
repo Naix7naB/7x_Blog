46e79cb70115d77ec7a5602e27ba2cdf0e3338da
Naix7naB <duemarsgh@foxmail.com>
2023-09-03 22:34:09 +0800

    feat: 路由(删除上传文件)

--- serve/routes/upload.js
+++ serve/routes/upload.js
@@ -8,20 +8,31 @@ const Response = require('../core/response')
 const { UPLOAD_PATH } = require('../config/base.config')
 const { resolveUrl } = require('../utils/helpers')
 
-const categoryMap = {
-    user: 'user',
-    article: 'article',
-    other: 'other'
+const categories = ['user', 'article', 'website', 'other']
+
+/**
+ * @function checkDirExistence
+ * @description 检查是否存在此类别文件夹
+ * @param {String} category 类别名称
+ * @return {String | Null} 如果存在此类别, 则返回该文件夹路径. 不存在则返回null
+ */
+function checkDirExistence(category) {
+    let dirPath = null
+    if (categories.includes(category)) {
+        dirPath = path.join(UPLOAD_PATH, category)
+        if (!fs.existsSync(dirPath)) {
+            fs.mkdirSync(dirPath)
+        }
+    }
+    return dirPath
 }
 
 const storage = multer.diskStorage({
     destination(req, file, next) {
         const field = req.params['category'] || null
-        const category = categoryMap[field] || null
-        assert(category, 422, '文件上传分类不正确')
-        const savePath = path.join(UPLOAD_PATH, category)
-        fs.existsSync(savePath) || fs.mkdirSync(savePath)
-        next(null, savePath)
+        const dirPath = checkDirExistence(field)
+        assert(dirPath, 422, '暂无此类别')
+        next(null, dirPath)
     },
     filename(req, file, next) {
         const { ext } = path.parse(file.originalname)
@@ -55,6 +66,22 @@ Router.post('/:category', upload.any(), async (req, res, next) => {
                 fileUrls
             }
         })
+    } catch (err) {
+        next(err)
+    }
+})
+
+// 删除文件
+Router.delete('/:category/:filename', async (req, res, next) => {
+    try {
+        const field = req.params['category'] || null
+        const filename = req.params['filename'] || null
+        const dirPath = checkDirExistence(field)
+        assert(dirPath, 422, '暂无此类别')
+        const filePath = path.join(dirPath, filename)
+        assert(fs.existsSync(filePath), 422, '该文件已被删除')
+        fs.unlinkSync(filePath)
+        Response.send(res, { message: '删除成功' })
     } catch (err) {
         next(err)
     }
