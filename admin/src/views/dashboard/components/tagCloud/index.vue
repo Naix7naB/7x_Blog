30d46602d68ed6594f0bf93973677e4daad48a96
Naix7naB <duemarsgh@foxmail.com>
2023-09-03 20:21:29 +0800

    feat: 词云组件

---
+++ admin/src/views/dashboard/components/tagCloud/index.vue
@@ -0,0 +1,54 @@
+<script>
+import TagCloud from 'TagCloud'
+
+export default {
+    props: {
+        width: {
+            type: String,
+            default: '100%'
+        },
+        height: {
+            type: String,
+            default: '300px'
+        }
+    },
+    data() {
+        return {
+            tagcloud: null,
+            texts: ['Vue', 'Nodejs', 'JavaScript', 'CSS'],
+            options: {
+                keep: false,
+                initSpeed: 'fast',
+                maxSpeed: 'fast',
+                containerClass: 'tag-cloud',
+                itemClass: 'tag-cloud--item',
+                useContainerInlineStyles: false
+            }
+        }
+    },
+    mounted() {
+        this.$nextTick(() => {
+            this.tagcloud = TagCloud(this.$refs.tagCloud, this.texts, this.options)
+        })
+    },
+    beforeDestroy() {
+        if (!this.tagcloud) return false
+        this.tagcloud.destroy()
+        this.tagcloud = null
+    }
+}
+</script>
+
+<template>
+    <div ref="tagCloud" :style="{ width: width, height: height }"></div>
+</template>
+
+<style lang="scss" scoped>
+:deep(.tag-cloud) {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+    color: #333;
+}
+</style>
