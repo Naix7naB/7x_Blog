e7fbdeae0101a5db651cacfe0535c8700b7c9ba0
Naix7naB <duemarsgh@foxmail.com>
2023-09-06 00:47:36 +0800

    feat: 操作按钮新增query和reset两个种类

--- admin/src/components/table/components/operationBtn/index.vue
+++ admin/src/components/table/components/operationBtn/index.vue
@@ -1,4 +1,32 @@
 <script>
+const buttonMap = {
+    edit: {
+        type: 'primary',
+        text: '编辑',
+        icon: 'el-icon-edit'
+    },
+    check: {
+        type: '',
+        text: '查看',
+        icon: 'el-icon-view'
+    },
+    delete: {
+        type: 'danger',
+        text: '删除',
+        icon: 'el-icon-delete'
+    },
+    query: {
+        type: 'primary',
+        text: '查找',
+        icon: 'el-icon-search'
+    },
+    reset: {
+        type: '',
+        text: '重置',
+        icon: 'el-icon-refresh'
+    }
+}
+
 export default {
     name: 'OperationBtn',
     functional: true,
@@ -6,25 +34,24 @@ export default {
         type: {
             type: String,
             default: 'edit'
+        },
+        size: {
+            type: String,
+            default: 'mini'
+        },
+        showIcon: {
+            type: Boolean,
+            default: false
         }
     },
     render(h, ctx) {
-        const { type } = ctx.props
-        const config = { size: 'mini' }
-        if (type === 'edit') {
-            config.type = 'primary'
-            config.text = '编辑'
-        }
-        if (type === 'check') {
-            config.type = ''
-            config.text = '查看'
-        }
-        if (type === 'delete') {
-            config.type = 'danger'
-            config.text = '删除'
+        const { type, size, showIcon } = ctx.props
+        const config = { size, ...buttonMap[type] }
+        if (showIcon === false) {
+            delete config.icon
         }
         Object.assign(ctx.data.attrs, config)
-        return <el-button { ...ctx.data } >{config.text}</el-button>
+        return <el-button { ...ctx.data } >{ config.text }</el-button>
     }
 }
 </script>
