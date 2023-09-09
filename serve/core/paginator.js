c39eb92576ad68941254b7020427c54cd0bd0e68
Naix7naB <duemarsgh@foxmail.com>
2023-09-10 01:14:49 +0800

    feat: 获取资源时, req.query.query字段来进行模糊搜索

--- serve/core/paginator.js
+++ serve/core/paginator.js
@@ -2,24 +2,23 @@ const qs = require('qs')
 const mongoPagination = require('mongoose-sex-page')
 
 const Populate = require('../plugins/populate')
+const { isEmptyObj } = require('../utils/helpers')
 
 module.exports = class Paginator {
-    static handleCondition(condition) {
-        if (typeof condition === 'undefined') return null
-        if (typeof condition === 'string') {
-            condition = qs.parse(condition)
-        }
-        if (condition && condition.q) {
-            const regexp = new RegExp(condition.q, 'gi')
-            condition = {
-                $or: [
-                    { title: { $regex: regexp } },
-                    { desciption: { $regex: regexp } },
-                    { content: { $regex: regexp } }
-                ]
+    static handleCondition(condition = {}, query = {}) {
+        condition = qs.parse(condition)
+        query = qs.parse(query)
+        if (query && !isEmptyObj(query)) {
+            query = {
+                $or: Object.entries(query).map(([key, val]) => {
+                    const regexp = new RegExp(val, 'gi')
+                    return {
+                        [key]: { $regex: regexp }
+                    }
+                })
             }
         }
-        return condition
+        return Object.assign(condition, query)
     }
 
     static handlePopulate(populate, model) {
@@ -29,14 +28,24 @@ module.exports = class Paginator {
         return populate
     }
 
-    static unpaging({ model, condition, sort = null, populate = null, select = null }) {
-        condition = Paginator.handleCondition(condition)
+    static unpaging({ model, condition, query, sort = null, populate = null, select = null }) {
+        condition = Paginator.handleCondition(condition, query)
         populate = Paginator.handlePopulate(populate, model)
         return model.find(condition).sort(sort).populate(populate).select(select).exec()
     }
 
-    static paging({ model, condition, page, size = 10, dis = null, sort = null, populate = null, select = null }) {
-        condition = Paginator.handleCondition(condition)
+    static paging({
+        model,
+        condition,
+        query,
+        page,
+        size = 10,
+        dis = null,
+        sort = null,
+        populate = null,
+        select = null
+    }) {
+        condition = Paginator.handleCondition(condition, query)
         populate = Paginator.handlePopulate(populate, model)
         return mongoPagination(model)
             .find(condition)
