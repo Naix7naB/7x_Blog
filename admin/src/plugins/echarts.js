a24ef2e658d1ce233d4e7d52d0a5aca7e9d6b885
Naix7naB <duemarsgh@foxmail.com>
2023-09-16 03:28:17 +0800

    feat: mixins添加对图表的操作

---
+++ admin/src/plugins/echarts.js
@@ -0,0 +1,9 @@
+import * as echarts from 'echarts/core'
+import { TooltipComponent, LegendComponent } from 'echarts/components'
+import { PieChart } from 'echarts/charts'
+import { LabelLayout } from 'echarts/features'
+import { CanvasRenderer } from 'echarts/renderers'
+
+echarts.use([TooltipComponent, LegendComponent, PieChart, CanvasRenderer, LabelLayout])
+
+export default echarts
--- admin/src/views/dashboard/components/mixins/index.js
+++ admin/src/views/dashboard/components/mixins/index.js
@@ -1,34 +1,26 @@
+import echarts from '@/plugins/echarts'
+
 import { debounce } from '@/utils'
 
 export default {
     data() {
         return {
+            $_chart: null,
             $_sidebarElm: null,
             $_resizeHandler: null
         }
     },
-    mounted() {
-        this.$_resizeHandler = debounce(() => {
-            if (this.chart) {
-                this.chart.resize()
-            }
-        }, 100)
-        this.$_initResizeEvent()
-        this.$_initSidebarResizeEvent()
-    },
-    beforeDestroy() {
-        this.$_destroyResizeEvent()
-        this.$_destroySidebarResizeEvent()
-    },
-    activated() {
-        this.$_initResizeEvent()
-        this.$_initSidebarResizeEvent()
-    },
-    deactivated() {
-        this.$_destroyResizeEvent()
-        this.$_destroySidebarResizeEvent()
-    },
     methods: {
+        $_initChart() {
+            this.$_chart = echarts.init(this.$refs.chart)
+        },
+        $_setChartOption(opt) {
+            this.$_chart.setOption(opt)
+        },
+        $_destroyChart() {
+            this.$_chart && this.$_chart.dispose()
+            this.$_chart = null
+        },
         $_initResizeEvent() {
             window.addEventListener('resize', this.$_resizeHandler)
         },
@@ -49,5 +41,28 @@ export default {
             this.$_sidebarElm &&
                 this.$_sidebarElm.removeEventListener('transitionend', this.$_sidebarResizeHandler)
         }
+    },
+    mounted() {
+        this.$_resizeHandler = debounce(() => {
+            if (this.$_chart) {
+                this.$_chart.resize()
+            }
+        }, 100)
+        this.$_initChart()
+        this.$_initResizeEvent()
+        this.$_initSidebarResizeEvent()
+    },
+    beforeDestroy() {
+        this.$_destroyChart()
+        this.$_destroyResizeEvent()
+        this.$_destroySidebarResizeEvent()
+    },
+    activated() {
+        this.$_initResizeEvent()
+        this.$_initSidebarResizeEvent()
+    },
+    deactivated() {
+        this.$_destroyResizeEvent()
+        this.$_destroySidebarResizeEvent()
     }
 }
