da54ac9758beaf573dc75fc935f835ecaf86f0d3
Naix7naB <duemarsgh@foxmail.com>
2023-09-18 00:12:05 +0800

    feat: 使用lodash库的防抖节流函数

--- admin/src/views/dashboard/components/mixins/index.js
+++ admin/src/views/dashboard/components/mixins/index.js
@@ -1,12 +1,12 @@
 import echarts from '@/plugins/echarts'
 
-import { debounce } from '@/utils'
+import { debounce } from 'lodash-es'
 
 export default {
     data() {
         return {
             $_chart: null,
-            $_sidebarElm: null,
+            $_sidebar: null,
             $_resizeHandler: null
         }
     },
@@ -33,21 +33,22 @@ export default {
             }
         },
         $_initSidebarResizeEvent() {
-            this.$_sidebarElm = document.getElementsByClassName('sidebar-container')[0]
-            this.$_sidebarElm &&
-                this.$_sidebarElm.addEventListener('transitionend', this.$_sidebarResizeHandler)
+            this.$_sidebar = document.getElementsByClassName('sidebar-container')[0]
+            this.$_sidebar &&
+                this.$_sidebar.addEventListener('transitionend', this.$_sidebarResizeHandler)
         },
         $_destroySidebarResizeEvent() {
-            this.$_sidebarElm &&
-                this.$_sidebarElm.removeEventListener('transitionend', this.$_sidebarResizeHandler)
+            this.$_sidebar &&
+                this.$_sidebar.removeEventListener('transitionend', this.$_sidebarResizeHandler)
         }
     },
     mounted() {
-        this.$_resizeHandler = debounce(() => {
+        this.$_resizeHandler = debounce(e => {
             if (this.$_chart) {
+                if (e.target.innerWidth <= 992) return false
                 this.$_chart.resize()
             }
-        }, 100)
+        }, 50)
         this.$_initChart()
         this.$_initResizeEvent()
         this.$_initSidebarResizeEvent()
