1f4ee835074d0031583c9d50ebc9676cf179a610
Naix7naB <duemarsgh@foxmail.com>
2023-09-20 01:42:20 +0800

    feat: 仪表盘页点击数据跳转路由

--- admin/src/views/dashboard/components/panelGroup/index.vue
+++ admin/src/views/dashboard/components/panelGroup/index.vue
@@ -20,30 +20,43 @@ export default {
                     prop: 'article_count',
                     text: '文章数',
                     icon: 'austral-sign',
-                    value: 0
+                    value: 0,
+                    path: '/article'
                 },
                 {
                     name: 'view',
                     prop: 'view_count',
                     text: '总访问量',
                     icon: 'chart-line',
-                    value: 0
+                    value: 0,
+                    path: ''
                 },
                 {
                     name: 'user',
                     prop: 'user_count',
                     text: '用户量',
                     icon: 'user-group',
-                    value: 0
+                    value: 0,
+                    path: '/user'
                 },
                 {
                     name: 'message',
                     prop: 'message_count',
                     text: '留言数',
                     icon: 'comment-dots',
-                    value: 0
+                    value: 0,
+                    path: '/news/message'
                 }
             ]
+        }
+    },
+    methods: {
+        onPanelClick(panel) {
+            if (panel.name === 'view') {
+                this.$message.warning('功能暂未开放')
+            } else {
+                this.$router.push(panel.path)
+            }
         }
     },
     created() {
@@ -63,7 +76,7 @@ export default {
 <template>
     <el-row :gutter="gutter">
         <el-col v-for="panel in panels" :key="panel.name" :xs="24" :sm="12" :lg="6">
-            <el-card>
+            <el-card @click.native="onPanelClick(panel)">
                 <div :class="['panel-icon', panel.name]">
                     <fa-icon :icon="['fas', panel.icon]" size="4x" />
                 </div>
