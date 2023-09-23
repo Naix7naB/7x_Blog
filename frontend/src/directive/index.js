5d1f8092abb5efa044d32645223c50e02e96a278
Naix7naB <duemarsgh@foxmail.com>
2023-09-23 08:38:57 +0800

    fix: 文章详情页图片预览样式

--- frontend/src/directive/index.js
+++ frontend/src/directive/index.js
@@ -2,6 +2,7 @@ import highlight from './highlight'
 import loading from './loading'
 import typed from './typed'
 import { directive as viewer } from 'v-viewer'
+import 'viewerjs/dist/viewer.min.css'
 
 export default {
     install(Vue) {
