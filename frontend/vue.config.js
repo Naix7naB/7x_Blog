20ab325b41d178aac2faf3829cdeb253de8d4a81
Naix7naB <duemarsgh@foxmail.com>
2023-09-27 06:04:35 +0800

    feat: vue-cli的打包配置

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,17 +1,41 @@
 const { defineConfig } = require('@vue/cli-service')
+
+const path = require('path')
+
+const port = process.env.port || process.env.npm_config_port || 8000 // dev port
+
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
+
 module.exports = defineConfig({
+    publicPath: '/',
+    outputDir: 'dist',
+    assetsDir: 'assets',
+    filenameHashing: true,
     transpileDependencies: true,
+    productionSourceMap: false,
+    integrity: false,
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@': resolve('/src')
+            }
+        }
+    },
     css: {
+        extract: true,
         loaderOptions: {
             scss: {
                 additionalData: `
-          @import "@/assets/scss/variable.scss";
-          @import "@/assets/scss/mixin.scss";
-        `
+                    @import "@/assets/scss/variable.scss";
+                    @import "@/assets/scss/mixin.scss";
+                `
             }
         }
     },
     devServer: {
-        port: 8000
+        port: port,
+        open: true
     }
 })
