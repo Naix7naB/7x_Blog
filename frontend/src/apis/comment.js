99ac3c5d6a6079414cf1ef5d324ac2b88f649666
Naix7naB <duemarsgh@foxmail.com>
2023-10-12 17:34:43 +0800

    feat: 获取评论列表api采用传参形式来获取不同的评论

--- frontend/src/apis/comment.js
+++ frontend/src/apis/comment.js
@@ -1,7 +1,7 @@
 import Request from '@/utils/request'
 
-/* 获取文章评论列表 */
-function getArticleComments({ page = 1, size = 20, aid } = {}) {
+/* 获取评论列表 */
+function getCommentList({ page = 1, size = 10, topic_type, topic_id } = {}) {
     return Request.requestForm({
         methodType: Request.GET,
         url: '/api/comment',
@@ -9,45 +9,12 @@ function getArticleComments({ page = 1, size = 20, aid } = {}) {
             page,
             size,
             condition: {
-                topic_type: 'article_comment',
-                topic_id: aid
-            },
-            populate: [
-                {
-                    path: 'reviewer',
-                    select: 'avatar nickname'
-                },
-                {
-                    path: 'mention',
-                    select: 'avatar nickname'
+                topic_type,
+                topic_id,
+                parent_id: {
+                    $type: 'null'
                 }
-            ]
-        }
-    })
-}
-
-/* 获取浏览板评论列表 */
-function getMessageComments({ page = 1, size = 20, mid } = {}) {
-    return Request.requestForm({
-        methodType: Request.GET,
-        url: '/api/comment',
-        data: {
-            page,
-            size,
-            condition: {
-                topic_type: 'message_comment',
-                topic_id: mid
-            },
-            populate: [
-                {
-                    path: 'reviewer',
-                    select: 'avatar nickname'
-                },
-                {
-                    path: 'mention',
-                    select: 'avatar nickname'
-                }
-            ]
+            }
         }
     })
 }
@@ -61,4 +28,4 @@ function leaveComment(data) {
     })
 }
 
-export { getArticleComments, getMessageComments, leaveComment }
+export { getCommentList, leaveComment }
